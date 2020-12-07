7da4e7e7fd8f20342a582423782c250055e49093
PiNE <53064235+FaultyPine@users.noreply.github.com>
2020-12-06 23:09:26 -0600

    fukin read the prompts plees

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,8 @@ pub fn main() {
         /* If the "marker" for being in the middle of an install doesn't exist, prompt for the update */
         if !Path::new("sd:/installing.tmpfile").exists() {
             skyline_web::DialogOk::ok(
-                "HDR will now be installed. This is a first-time setup and will take some time.
-                The screen will be blank for a while, but don't worry, this is normal. Please be patient. 
+                "ATTENTION: HDR will now be installed. This is a first-time setup and will take some time.
+                The screen will be blank for a while, but don't worry, THIS IS NORMAL. Please be patient. 
                 On the next screen you'll be prompted for an update, please select 'Yes'."
             );
         }
